e4b42e66b73ea686b79f87bef201b8b506b5054c
Virusmater <virusmater@gmail.com>
2020-06-22 19:59:44 +0200

    add flask_talisman

--- togger/__init__.py
+++ togger/__init__.py
@@ -3,8 +3,15 @@ import os
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_talisman import Talisman
+
+csp = {
+    'script-src': ['\'unsafe-inline\'', '\'self\'', 'code.jquery.com', 'cdn.jsdelivr.net',
+                   'stackpath.bootstrapcdn.com', 'cdnjs.cloudflare.com']
+}
 
 application = Flask(__name__)
+talisman = Talisman(application, content_security_policy=csp)
 application.config.from_object('togger.default_settings')
 if os.environ.get("SECRET_KEY"):
     application.secret_key = os.environ.get("SECRET_KEY")
